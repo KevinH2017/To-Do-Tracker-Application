f73266003ed08ca36cddbe5f54ff20130a6df7fc
KevinH <khui98@gmail.com>
2023-10-29 14:51:56 -0400

    Update web.py
    Implemented Adding a new task to check boxes

--- app1/web.py
+++ app1/web.py
@@ -3,7 +3,14 @@
 import streamlit as st
 from modules import functions
 
+
+
 todos = functions.read_file()
+
+def add_task():
+    task = st.session_state["new_task"] + "\n"
+    todos.append(task)
+    functions.write_file(todos)
 
 st.title("My Tasks-To-Do App")
 st.subheader("This is my Tasks-To-Do app.")
@@ -12,4 +19,8 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a Task", placeholder="Add a new task")
+st.text_input(label="Enter a Task", placeholder="Add a new task", on_change=add_task, key="new_task")
+
+print("Hello")
+
+st.session_state
