c6b8da0113b35a38b6d8b562fe615661b20291cd
KevinH <khui98@gmail.com>
2023-10-29 15:27:36 -0400

    Changed web to web2.
    Changed web to web2.
    Checkbox list now includes task numbers.

--- app1/web2.py
+++ app1/web2.py
@@ -3,8 +3,6 @@
 import streamlit as st
 from modules import functions
 
-
-
 todos = functions.read_file()
 
 def add_task():
@@ -16,11 +14,12 @@ st.title("My Tasks-To-Do App")
 st.subheader("This is my Tasks-To-Do app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, task in enumerate(todos):
+    checkbox = st.checkbox(str(index + 1) + ' - ' + str(task), key=task)
+    if checkbox:
+        todos.pop(index)
+        functions.write_file(todos)
+        del st.session_state[task]
+        st.rerun()
 
 st.text_input(label="Enter a Task", placeholder="Add a new task", on_change=add_task, key="new_task")
-
-print("Hello")
-
-st.session_state
